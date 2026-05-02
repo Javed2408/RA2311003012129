e17c37e57fb3f69a7bdabcac419d4ebfb774e52b
Javed <jk42273@gmail.com>
2026-05-02 12:32:18 +0530

    added output screenshots

---
+++ vehicle_maintenance_scheduler/src/routes/schedulerRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {
+    scheduleTasks
+} = require(
+    "../controllers/schedulerController"
+);
+
+router.get(
+    "/schedule",
+    scheduleTasks
+);
+
+module.exports = router;
